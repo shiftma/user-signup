4e9a17d6d70e8bdaf645e8b9f84770df39ba9932
Maria Eichman <m.levanuck@gmail.com>
2017-11-23 10:31:07 -0600

    Implemented email validation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template
 import html
 import os
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
@@ -28,7 +29,7 @@ def validate():
     username = str(request.form['username'])
     password = str(request.form['password'])
     verify =str(request.form['verify'])
-    email = request.form['email']
+    email = str(request.form['email'])
 
     username_error = ''
     password_error =''
@@ -41,16 +42,17 @@ def validate():
     if is_empty(password) or not valid_length(password):
         password_error = 'That\'s not a valid password'
 
-    if is_empty(verify):
+    if is_empty(verify) or password != verify:
         verify_error = 'Passwords don\'t match'
 
-    if  password != verify:
-        verify_error = 'Passwords don\'t match'
+    if not re.match(r"[^@]+@[^@]+\.[^@]+[^\S]", email): 
+        email_error = 'Email is not valid'   
 
+    # if no error occurred return welcome page, else return error message
     if not username_error and not password_error and not verify_error:
         return render_template('welcome.html', username=username)
     else:
-        return render_template('index.html', user_error = username_error, password_error = password_error, verify_error = verify_error)
+        return render_template('index.html', user_error = username_error, password_error = password_error, verify_error = verify_error, email_error = email_error)
 
 
 app.run()
